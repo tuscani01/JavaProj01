1b779a0cf496b667d378bff8aed5007996b793b6
kosmo111 <kosmo111@DESKTOP-4O3BLID>
2020-03-26 18:39:34 +0900

    ..

--- src/PhonebookVer05.java
+++ src/PhonebookVer05.java
@@ -1,6 +1,6 @@
 import ver01.Phoneinfo;
 
-public class PhonebookVer01
+public class PhonebookVer05
 {
 
 	public static void main(String[] args)
@@ -8,7 +8,9 @@ public class PhonebookVer01
 	
 		Phoneinfo p1 = new Phoneinfo("토니스탁", "010-1234-6575", "70-01-05");
 		Phoneinfo p2 = new Phoneinfo("브루스배너", "010-1256-6775");
-		Phoneinfo p3 = new Phoneinfo("브루스웨인", "010-1256-6775");
+		Phoneinfo p3 = new Phoneinfo("리처드해먼드", "010-5474-0909", "69-12-19");
+		Phoneinfo p4 = new Phoneinfo("제임스메이", "010-8484-9564", "63-01-16");
+		Phoneinfo p5 = new Phoneinfo("제레미클락슨", "010-9898-5674", "60-04-11");
 		
 		p1.showPhoneInfo();
 		p2.showPhoneInfo(); //생년월일 입력X
